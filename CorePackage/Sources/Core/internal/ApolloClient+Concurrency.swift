60a3ebd322abdd284de8a4b66586a25646ce7219
terry <teruhito.wakae@chatwork.com>
2023-08-22 16:59:10 +0900

    Translatorに準拠しているQuery.DataはModelに変換してから返却できるように修正 ※このコミットはビルドに失敗します。

--- CorePackage/Sources/Core/internal/ApolloClient+Concurrency.swift
+++ CorePackage/Sources/Core/internal/ApolloClient+Concurrency.swift
@@ -8,12 +8,20 @@ extension ApolloClient {
         cachePolicy: CachePolicy = .default,
         contextIdentifier: UUID? = nil,
         queue: DispatchQueue = .main
-    ) async throws -> Query.Data? {
+    ) async throws -> Query.Data.Model where Query.Data: Translator {
         return try await withCheckedThrowingContinuation { continuation in
             fetch(query: query, cachePolicy: cachePolicy, contextIdentifier: contextIdentifier, queue: queue) {  result in
                 switch result {
                 case .success(let graphQLResult):
-                    continuation.resume(returning: graphQLResult.data)
+                    guard let data = graphQLResult.data else {
+                        continuation.resume(throwing: TranslateError.null)
+                        return
+                    }
+                    do {
+                        continuation.resume(returning: try data.toModel())
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
                 case .failure(let error):
                     print(error.localizedDescription)
                     continuation.resume(throwing: error)
--- CorePackage/Sources/Core/internal/ApollotStore+Concurrency.swift
+++ CorePackage/Sources/Core/internal/ApollotStore+Concurrency.swift
@@ -3,14 +3,18 @@ import Apollo
 import ApolloAPI
 
 extension ApolloStore {
-    public func withinReadTransaction<Query>(
+    func withinReadTransaction<Query>(
       _ query: Query
-    ) async throws -> Query.Data where Query: GraphQLQuery {
+    ) async throws -> Query.Data.Model where Query: GraphQLQuery, Query.Data: Translator {
         return try await withCheckedThrowingContinuation { continuation in
             withinReadTransaction({ transaction in
                 do {
                     let data = try transaction.read(query: query)
-                    continuation.resume(returning: data)
+                    do {
+                        continuation.resume(returning: try data.toModel())
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
                 } catch {
                     continuation.resume(throwing: error)
                 }
