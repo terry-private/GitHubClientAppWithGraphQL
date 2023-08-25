4c9c7b51628460e17bd5d0b97f128f5d703e267d
terry <teruhito.wakae@chatwork.com>
2023-08-25 11:43:11 +0900

    EnumTranslator作成

---
+++ CorePackage/Sources/Core/internal/EnumTranslator/EnumTranslator.swift
@@ -0,0 +1,51 @@
+import Foundation
+import GitHubSchema
+
+protocol EnumTranslatorProtocol {
+    static func repositories(from queryData: SearchRepositoriesQuery.Data) throws -> [Repository]
+}
+
+
+enum EnumTranslator: EnumTranslatorProtocol {
+    static func repositories(from queryData: SearchRepositoriesQuery.Data) throws -> [Repository] {
+        guard let edges = queryData.search.edges else {
+            throw TranslateError.null
+        }
+        return try edges.map {
+            guard let repository = try $0?.node?.asRepository?.toModel()  else {
+                throw TranslateError.null
+            }
+            return repository
+        }
+    }
+}
+
+private extension EnumTranslator {
+    static func repository(from queryRepository: SearchRepositoriesQuery.Data.Search.Edge.Node.AsRepository) throws -> Repository {
+        let owner = try queryRepository.owner.toModel()
+        return Repository(
+            id: Repository.ID(rawValue: queryRepository.id),
+            name: queryRepository.name,
+            stargazerCount: queryRepository.stargazerCount,
+            owner: owner
+        )
+    }
+    
+    static func owner(from queryOwner: SearchRepositoriesQuery.Data.Search.Edge.Node.AsRepository.Owner) throws -> Owner {
+        if let organization = queryOwner.asOrganization {
+            return Owner(
+                id: Owner.ID(rawValue: organization.id),
+                avatarURL: URL(string: organization.avatarUrl),
+                name: organization.name
+            )
+        }
+        if let user = queryOwner.asUser {
+            return Owner(
+                id: Owner.ID(rawValue: user.id),
+                avatarURL: URL(string: user.avatarUrl),
+                name: user.name
+            )
+        }
+        throw TranslateError.null
+    }
+}
