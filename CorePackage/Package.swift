20d7749db6e5be13ca87ea1a7ad8a3af88778869
terry <teruhito.wakae@chatwork.com>
2023-09-08 16:07:02 +0900

    Packageの書き方を整理

--- CorePackage/Package.swift
+++ CorePackage/Package.swift
@@ -3,10 +3,61 @@
 
 import PackageDescription
 
-private extension PackageDescription.Target.Dependency {
-    static let apollo: Self = .product(name: "Apollo", package: "apollo-ios")
-    static let apolloSQLite: Self = .product(name: "ApolloSQLite", package: "apollo-ios")
-    static let gitHubSchema: Self = .product(name: "GitHubSchema", package: "GitHubSchema")
+// MARK: - 外部パッケージ
+enum ExternalPackage: CaseIterable {
+    case apollo
+    case gitHubSchema
+    
+    var dependency: Dependency {
+        switch self {
+        case .apollo:
+            return .init(
+                name: "apollo-ios",
+                package: .package(
+                    url: "https://github.com/apollographql/apollo-ios.git",
+                    .upToNextMajor(from: "1.0.0")
+                )
+            )
+        case .gitHubSchema:
+            return .init(
+                name: "GitHubSchema",
+                package: .package(path: "GitHubSchema")
+            )
+        }
+    }
+    
+    struct Dependency {
+        var name: String
+        var package: Package.Dependency
+    }
+}
+
+//　MARK: - 外部モジュール
+private extension Target.Dependency {
+    static let apollo: Target.Dependency = .product(name: "Apollo", .apollo)
+    static let apolloSQLite: Target.Dependency = .product(name: "ApolloSQLite", .apollo)
+    static let gitHubSchema: Target.Dependency = .product(name: "GitHubSchema", .gitHubSchema)
+}
+
+//　MARK: - 内部モジュール
+enum DependencyModule: String, CaseIterable {
+    case core = "Core"
+    
+    var target: Target {
+        switch self {
+        case .core:
+            return .target(
+                name: rawValue,
+                dependencyModules: [
+                ],
+                dependencies: [
+                    .apollo,
+                    .apolloSQLite,
+                    .gitHubSchema
+                ]
+            )
+        }
+    }
 }
 
 let package = Package(
@@ -14,29 +65,51 @@ let package = Package(
     platforms: [
         .iOS("16.1"),
     ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Core",
-            targets: ["Core"]),
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apollographql/apollo-ios.git",
-            .upToNextMajor(from: "1.0.0")
-        ),
-        .package(path: "GitHubSchema")
-    ],
-    targets: [
-        .target(
-            name: "Core",
-            dependencies: [
-                .apollo,
-                .apolloSQLite,
-                .gitHubSchema
-            ]),
-        .testTarget(
-            name: "CorePackageTests",
-            dependencies: ["Core"]),
-    ]
+    products: DependencyModule.allCases.map { .library(name: $0.rawValue, targets: [$0.rawValue])},
+    dependencies: ExternalPackage.allCases.map { $0.dependency.package },
+    targets: DependencyModule.allCases.map { $0.target }
 )
+
+// MARK: - Configure Extensions
+private extension DependencyModule {
+    var dependency: Target.Dependency { .byName(name: self.rawValue) }
+}
+
+private extension Target {
+    static func target(
+        name: String,
+        dependencyModules: [DependencyModule] = [], // 標準メソッドに追加
+        dependencies: [Dependency] = [],
+        path: String? = nil,
+        exclude: [String] = [],
+        sources: [String]? = nil,
+        resources: [Resource]? = nil,
+        publicHeadersPath: String? = nil,
+        cSettings: [CSetting]? = nil,
+        cxxSettings: [CXXSetting]? = nil,
+        swiftSettings: [SwiftSetting]? = nil,
+        linkerSettings: [LinkerSetting]? = nil,
+        plugins: [PluginUsage]? = nil
+    ) -> Target {
+        return Target.target(
+            name: name,
+            dependencies: dependencyModules.map { $0.dependency } + dependencies,
+            path: path,
+            exclude: exclude,
+            sources: sources,
+            resources: resources,
+            publicHeadersPath: publicHeadersPath,
+            cSettings: cSettings,
+            cxxSettings: cxxSettings,
+            swiftSettings: swiftSettings,
+            linkerSettings: linkerSettings,
+            plugins: plugins
+        )
+    }
+}
+
+private extension Target.Dependency {
+    static func product(name: String, _ externalPackage: ExternalPackage) -> PackageDescription.Target.Dependency {
+        .product(name: name, package: externalPackage.dependency.name)
+    }
+}
