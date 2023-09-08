af45821d3c6517c71ad66847be0f3e2f891e4d47
terry <teruhito.wakae@chatwork.com>
2023-09-09 00:13:42 +0900

    queryの並び順を修正

--- ApolloSchema/GitHubSchema/Sources/Operations/Queries/GetRepositoryQuery.graphql.swift
+++ ApolloSchema/GitHubSchema/Sources/Operations/Queries/GetRepositoryQuery.graphql.swift
@@ -7,7 +7,7 @@ public class GetRepositoryQuery: GraphQLQuery {
   public static let operationName: String = "GetRepository"
   public static let operationDocument: ApolloAPI.OperationDocument = .init(
     definition: .init(
-      #"query GetRepository($name: String!, $owner: String!) { repository(name: $name, owner: $owner) { __typename id name owner { __typename ... on User { id name } ... on Organization { id name } avatarUrl id } description forkCount primaryLanguage { __typename id name color } stargazerCount url watchers { __typename totalCount } } }"#
+      #"query GetRepository($name: String!, $owner: String!) { repository(name: $name, owner: $owner) { __typename id name description url forkCount stargazerCount watchers { __typename totalCount } owner { __typename id avatarUrl ... on User { name } ... on Organization { name } } primaryLanguage { __typename id name color } } }"#
     ))
 
   public var name: String
@@ -53,32 +53,49 @@ public class GetRepositoryQuery: GraphQLQuery {
         .field("__typename", String.self),
         .field("id", GitHubSchema.ID.self),
         .field("name", String.self),
-        .field("owner", Owner.self),
         .field("description", String?.self),
-        .field("forkCount", Int.self),
-        .field("primaryLanguage", PrimaryLanguage?.self),
-        .field("stargazerCount", Int.self),
         .field("url", GitHubSchema.URI.self),
+        .field("forkCount", Int.self),
+        .field("stargazerCount", Int.self),
         .field("watchers", Watchers.self),
+        .field("owner", Owner.self),
+        .field("primaryLanguage", PrimaryLanguage?.self),
       ] }
 
       public var id: GitHubSchema.ID { __data["id"] }
       /// The name of the repository.
       public var name: String { __data["name"] }
-      /// The User owner of the repository.
-      public var owner: Owner { __data["owner"] }
       /// The description of the repository.
       public var description: String? { __data["description"] }
-      /// Returns how many forks there are of this repository in the whole network.
-      public var forkCount: Int { __data["forkCount"] }
-      /// The primary language of the repository's code.
-      public var primaryLanguage: PrimaryLanguage? { __data["primaryLanguage"] }
-      /// Returns a count of how many stargazers there are on this object
-      public var stargazerCount: Int { __data["stargazerCount"] }
       /// The HTTP URL for this repository
       public var url: GitHubSchema.URI { __data["url"] }
+      /// Returns how many forks there are of this repository in the whole network.
+      public var forkCount: Int { __data["forkCount"] }
+      /// Returns a count of how many stargazers there are on this object
+      public var stargazerCount: Int { __data["stargazerCount"] }
       /// A list of users watching the repository.
       public var watchers: Watchers { __data["watchers"] }
+      /// The User owner of the repository.
+      public var owner: Owner { __data["owner"] }
+      /// The primary language of the repository's code.
+      public var primaryLanguage: PrimaryLanguage? { __data["primaryLanguage"] }
+
+      /// Repository.Watchers
+      ///
+      /// Parent Type: `UserConnection`
+      public struct Watchers: GitHubSchema.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: ApolloAPI.ParentType { GitHubSchema.Objects.UserConnection }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .field("totalCount", Int.self),
+        ] }
+
+        /// Identifies the total count of items in the connection.
+        public var totalCount: Int { __data["totalCount"] }
+      }
 
       /// Repository.Owner
       ///
@@ -90,15 +107,15 @@ public class GetRepositoryQuery: GraphQLQuery {
         public static var __parentType: ApolloAPI.ParentType { GitHubSchema.Interfaces.RepositoryOwner }
         public static var __selections: [ApolloAPI.Selection] { [
           .field("__typename", String.self),
-          .field("avatarUrl", GitHubSchema.URI.self),
           .field("id", GitHubSchema.ID.self),
+          .field("avatarUrl", GitHubSchema.URI.self),
           .inlineFragment(AsUser.self),
           .inlineFragment(AsOrganization.self),
         ] }
 
+        public var id: GitHubSchema.ID { __data["id"] }
         /// A URL pointing to the owner's public avatar.
         public var avatarUrl: GitHubSchema.URI { __data["avatarUrl"] }
-        public var id: GitHubSchema.ID { __data["id"] }
 
         public var asUser: AsUser? { _asInlineFragment() }
         public var asOrganization: AsOrganization? { _asInlineFragment() }
@@ -113,13 +130,12 @@ public class GetRepositoryQuery: GraphQLQuery {
           public typealias RootEntityType = GetRepositoryQuery.Data.Repository.Owner
           public static var __parentType: ApolloAPI.ParentType { GitHubSchema.Objects.User }
           public static var __selections: [ApolloAPI.Selection] { [
-            .field("id", GitHubSchema.ID.self),
             .field("name", String?.self),
           ] }
 
-          public var id: GitHubSchema.ID { __data["id"] }
           /// The user's public profile name.
           public var name: String? { __data["name"] }
+          public var id: GitHubSchema.ID { __data["id"] }
           /// A URL pointing to the owner's public avatar.
           public var avatarUrl: GitHubSchema.URI { __data["avatarUrl"] }
         }
@@ -134,13 +150,12 @@ public class GetRepositoryQuery: GraphQLQuery {
           public typealias RootEntityType = GetRepositoryQuery.Data.Repository.Owner
           public static var __parentType: ApolloAPI.ParentType { GitHubSchema.Objects.Organization }
           public static var __selections: [ApolloAPI.Selection] { [
-            .field("id", GitHubSchema.ID.self),
             .field("name", String?.self),
           ] }
 
-          public var id: GitHubSchema.ID { __data["id"] }
           /// The organization's public profile name.
           public var name: String? { __data["name"] }
+          public var id: GitHubSchema.ID { __data["id"] }
           /// A URL pointing to the owner's public avatar.
           public var avatarUrl: GitHubSchema.URI { __data["avatarUrl"] }
         }
@@ -166,23 +181,6 @@ public class GetRepositoryQuery: GraphQLQuery {
         public var name: String { __data["name"] }
         /// The color defined for the current language.
         public var color: String? { __data["color"] }
-      }
-
-      /// Repository.Watchers
-      ///
-      /// Parent Type: `UserConnection`
-      public struct Watchers: GitHubSchema.SelectionSet {
-        public let __data: DataDict
-        public init(_dataDict: DataDict) { __data = _dataDict }
-
-        public static var __parentType: ApolloAPI.ParentType { GitHubSchema.Objects.UserConnection }
-        public static var __selections: [ApolloAPI.Selection] { [
-          .field("__typename", String.self),
-          .field("totalCount", Int.self),
-        ] }
-
-        /// Identifies the total count of items in the connection.
-        public var totalCount: Int { __data["totalCount"] }
       }
     }
   }
