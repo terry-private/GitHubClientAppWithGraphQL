2981866c1375c9ba510800a6dd106001712759db
terry <teruhito.wakae@chatwork.com>
2023-09-09 00:13:55 +0900

    Lauguage作成

---
+++ CorePackage/Sources/Core/Model/Language.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct Language: Identifiable, Equatable {
+    public struct ID: RawRepresentable, Hashable, Codable {
+        public var rawValue: String
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+    public var id: ID
+    public var name: String
+    public var hexColor: String
+}
